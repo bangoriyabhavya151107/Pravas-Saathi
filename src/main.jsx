bb3cc091dccfaf9ce8dc22b4048b6572f3e3093f
hp724227-harshit <h.p.724227@gmail.com>
2026-08-22 15:04:01 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+import { AuthProvider }
+  from './components/AuthProvider'
+
+import App from './App'
+
+import './index.css'
+
+createRoot(
+  document.getElementById('root')
+).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
